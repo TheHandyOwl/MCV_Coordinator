8ef7a10f22110d43f2aa3a0d61d4fb54990616bd
The Handy Owl <thehandyowl.io@gmail.com>
2020-06-15 20:22:21 +0200

    Initial Commit

---
+++ MVC_Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC_Coordinator
+//
+//  Created by Carlos on 15/06/2020.
+//  Copyright © 2020 TestCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
